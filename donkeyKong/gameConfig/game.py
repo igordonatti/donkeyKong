e09c3b11052b977632472eba7ae3071f80bc81bc
Eumoitinho <joao.silva489@academico.ufgd.edu.br>
2024-06-15 11:40:03 -0400

    Comentários foram adicionados em cada parte dos códigos

--- donkeyKong/gameConfig/game.py
+++ donkeyKong/gameConfig/game.py
@@ -1,42 +1,55 @@
+# Importa o módulo pygame para criar jogos e interfaces gráficas
 import pygame
+# Importa a classe InitialMap do pacote maps para definir o mapa inicial do jogo
 from ..maps.initialmap import InitialMap
+# Importa a classe Display do pacote atual para gerenciar a exibição do jogo
 from .display import Display
+# Importa a classe Mario do pacote sprites para criar e gerenciar o personagem Mario
 from ..sprites.mario import Mario
+# Importa o módulo customtkinter para criar interfaces gráficas personalizadas
 from customtkinter import *
+# Importa a classe App do pacote Menu para gerenciar o menu do jogo
 from .Menu.App import App
 
-# O jogo se inicia aqui
+# Define a classe Game, que é a classe principal para executar o jogo
 class Game():
+    # Método construtor da classe Game
     def __init__(self):
-        self.fps = 60
-        self.display = Display()
+        self.fps = 60  # Define a taxa de quadros por segundo do jogo
+        self.display = Display()  # Cria uma instância de Display para gerenciar a exibição
         
-        self.menu = None
+        self.menu = None  # Inicializa o menu como None, será definido posteriormente
         
+        # Define a tela do jogo com as dimensões especificadas em Display
         self.screen = pygame.display.set_mode((self.display.width, self.display.height))
+        # Cria uma instância de InitialMap, que define o mapa inicial do jogo
         self.map = InitialMap(self.screen, self.display.section_width, self.display.section_height, self.display.slope)
       
+        # Obtém a posição inicial de Mario no mapa
         start_x, start_y = self.map.get_initial_mario_position()
+        # Cria uma instância de Mario na posição inicial obtida
         self.mario = Mario(start_x, start_y, self.map.platforms)
         
-        self.clock = pygame.time.Clock()
-        self.keys = pygame.key.get_pressed()
+        self.clock = pygame.time.Clock()  # Cria um relógio para controlar o tempo do jogo
+        self.keys = pygame.key.get_pressed()  # Obtém o estado atual das teclas pressionadas
         
+    # Método para iniciar o jogo, destruindo o menu se existir e chamando o método run
     def start_game(self):
       if self.menu:
-        self.menu.destroy()
-      self.run()
+        self.menu.destroy()  # Destrói o menu se ele existir
+      self.run()  # Inicia o loop principal do jogo
 
+    # Método run contém o loop principal do jogo
     def run(self):        
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+        while True:  # Loop infinito para manter o jogo rodando
+            for event in pygame.event.get():  # Itera sobre todos os eventos do pygame
+                if event.type == pygame.QUIT:  # Se o evento for de saída, fecha o jogo
                     pygame.quit()
                     exit()
 
-            self.screen.fill((0, 0, 0))
-            self.map.draw()  # Chama draw aqui, passando self.screen
-            self.mario.movement()
-            self.mario.drawMario(self.screen)
-            pygame.display.update()
-            self.clock.tick(60)
+            self.screen.fill((0, 0, 0))  # Limpa a tela preenchendo-a com preto
+            self.map.draw()  # Desenha o mapa na tela
+            self.mario.movement()  # Atualiza o movimento de Mario
+            self.mario.drawMario(self.screen)  # Desenha Mario na tela
+            pygame.display.update()  # Atualiza a tela com o que foi desenhado
+            self.clock.tick(60)  # Controla o FPS para que seja constante
--- donkeyKong/maps/initialmap.py
+++ donkeyKong/maps/initialmap.py
@@ -1,29 +1,37 @@
 from ..sprites.ladder import Ladder
 from ..sprites.bridge import Bridge
 
+# Definição da classe InitialMap.
 class InitialMap():
+    # Método construtor da classe, inicializa uma instância de InitialMap.
     def __init__(self, screen, section_width, section_height, slope):
+        # Atribuição dos parâmetros do construtor às variáveis de instância.
         self.screen = screen
         self.section_width = section_width
         self.section_height = section_height
         self.slope = slope
         
+        # Inicialização de listas para armazenar objetos de pontes, plataformas, escadas e corpos de escada.
         self.bridge_objs = []
         self.platforms = []
         self.climbers = []
         self.ladder_objs = []
         
+        # Criação dos níveis do jogo, pontes e escadas.
         self.levels = self.create_levels()
         self.create_bridges() 
         self.create_ladders()
 
+    # Método para criar os níveis do jogo, definindo a posição das pontes e escadas.
     def create_levels(self):
+        # Cálculo das posições iniciais para cada linha de pontes e escadas.
         start_y = self.screen.get_height() - 2 * self.section_height
         row2_y = start_y - 4 * self.section_height
         row3_y = row2_y - 7 * self.slope - 3 * self.section_height
         row4_y = row3_y - 4 * self.section_height
         row5_y = row4_y - 7 * self.slope - 3 * self.section_height
         row6_y = row5_y - 4 * self.section_height
+        # Retorna uma lista de dicionários contendo as posições das pontes e escadas.
         return [{'bridges': [
             (1, start_y, 15), 
             (16, start_y - self.slope, 3),
@@ -87,32 +95,38 @@ class InitialMap():
                        (10, row6_y - 17 * self.slope, 2), (12, -5, 13), (10, -5, 13)]
         }]
 
+    # Método para criar objetos de ponte a partir das informações dos níveis.
     def create_bridges(self):
         bridges = self.levels[0]['bridges']
         for bridge in bridges:
             x, y, length = bridge
+            # Criação de um novo objeto Bridge e adição às listas de objetos de ponte e plataformas.
             new_bridge = Bridge(x * self.section_width, y, length, self.section_width, self.section_height)
             self.bridge_objs.append(new_bridge)
             self.platforms.append(new_bridge.top)
 
+    # Método para criar objetos de escada a partir das informações dos níveis.
     def create_ladders(self):
         ladders = self.levels[0]['ladders']
         for ladder in ladders:
             x, y, length = ladder
+            # Criação de um novo objeto Ladder e adição às listas de objetos de escada e corpos de escada.
             new_ladder = Ladder(x * self.section_width, y, length, self.section_width, self.section_height)
             self.ladder_objs.append(new_ladder)
             if length >= 3:
                 self.climbers.append(new_ladder.body)
 
+    # Método para desenhar as pontes e escadas na tela.
     def draw(self):
         for bridge in self.bridge_objs:
             bridge.draw(self.screen)
         for ladder in self.ladder_objs:
             ladder.draw(self.screen)
 
+    # Método para obter a posição inicial de Mario no jogo.
     def get_initial_mario_position(self):
-      first_bridge = self.levels[0]['bridges'][0]
-      x, y, _ = first_bridge
-      return x * self.section_width, y
+        first_bridge = self.levels[0]['bridges'][0]
+        x, y, _ = first_bridge
+        return x * self.section_width, y
 
         
